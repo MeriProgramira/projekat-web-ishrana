9b8f7ff5280d5d8eb1f1ee0011540c4b474a395c
Indira147 <indiranuhanovic0@gmail.com>
2020-11-28 09:00:54 +0100

    Izbrisala linkove

--- resources/views/pages/vegetable.blade.php
+++ resources/views/pages/vegetable.blade.php
@@ -9,40 +9,34 @@
 <div class="container">
 		<h1>Zdrava ishrana</h1>
 		<div class="row">
-			<div id="povrceslika" class="col-lg-9 col-md-9 col-xs-12">
+			<div id="povrceslika"class="col-lg-9 col-md-9 col-xs-12">
 				<img src="https://radio.hrt.hr/data/article/127338_a257bb1081d3139d7627.jpg ">
-				<p>Pravilna ishrana je osnovni faktor optimalnog rasta i razvoja čovečijeg organizma i direkto utiče na njegovu radnu sposobnost i dužinu života. Nažalost, današnju ishranu karakterišu nepravilni i nedovoljno izbalansirani obroci. U njima ima previše masnoća, mesa, hleba a malo mlečnih proizvoda, voća i povrća. Nepravilna ishrana je faktor rizika za nastanak različitih oboljenja. Zbog toga trpi naš čitav organizam, a veoma je lako mnoge loše komplikacije izbeći jednostavnom promenom ishrane. Ako se prepoznajete u ovoj priči, odlučite da stavite tačku na to.
+			</div>	
+				<div id="tekst" class="col-lg-3 col-md-3 col-xs-12">
+				<p>Pravilna ishrana je osnovni faktor optimalnog rasta i razvoja čovečijeg organizma i direkto utiče na njegovu radnu sposobnost i dužinu života. Nažalost, današnju ishranu karakterišu nepravilni i nedovoljno izbalansirani obroci.Nepravilna ishrana je faktor rizika za nastanak različitih oboljenja. Zbog toga trpi naš čitav organizam, a veoma je lako mnoge loše komplikacije izbeći jednostavnom promenom ishrane. Ako se prepoznajete u ovoj priči, odlučite da stavite tačku na to.
 				Na našim stranicama, naći ćete savjete o izbalansiranoj ishrani,i kontakte za lokalne proizvođače zdravog voća i povrća iz ekološkog uzgoja.
-				Čovek je upotrebljavao povrće u svojoj ishrani još u najranijim danima svog postanka. 
-				Hranljva vrednost povrća se ogleda u velukom bogatstvu  skrob, glukoza i celuloza, te  minerala i vitamina. Zbog osetljivosti vitamina, povrće se mora čuvati u prohladnim i tamnim prostorijama. Zahvaljujući sadržaju fitoncida, koji imaju antibiotičke osobine, pojedina povrća (beli i crni luk, mrkva, mirođija i dr.) imaju lekovita svojstva. Njihova sposobnost da unoštavaju nepoželjne mikroorganizme koristi se za konzervisanje prehrabenih proizvoda, kao što su: turšije, konzerve od povrća i mesa itd.
-				Pojedine vrste povrća sadrže etarska ulja, sastojke koji lako isparavaju, imaju prijatan miris, a nalaze se u listićima mirođije ili u korenu i listu celera i peršuna, i stavljaju se u jela radi arome. 
-				Da bi se povrće moglo koristiti u toku cele godine, vrši se konzervisanje povrća.</p>
+				 
+				</p>
 			</div >
-			<div id="lokalniproizvodjaci" class="col-lg-3 col-md-3 col-xs-12">
-				<a href="#1" target="_blank">proizvodjac1</a><br>
-				<a href="#2" target="_blank">proizvodjac2</a><br>
-				<a href="#3" target="_blank">proizvodjac3</a><br>
-			</div>
 		</div>
 			<div class="row">
-						<div class ="col-md-4">
+						<div class ="col-lg-4 col-md-4 col-xs-12">
 							<h3>Proizvodi iz plasteničkog uzgoja</h3>
 							<img src="https://www.z-company.nl/wp-content/uploads/2019/06/spinach-300x300.jpg">
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque ornare aenean euismod elementum nisi. Feugiat nibh sed pulvinar proin. At auctor urna nunc id. Convallis a cras semper auctor neque vitae tempus quam pellentesque.</p>
-							<button id="vise">U ponudi..</button>
-						</div>
+							</div>
 							
-						<div class ="col-md-4">
+						<div class ="col-lg-4 col-md-4 col-xs-12">
 							<h3>Organsko povrće</h3>
 							<img src="https://agrimatco.hr/proizvodi/sjeme-povrca/lukovicasto-povrce/luk/lorenzos-f1.jpg">
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque ornare aenean euismod elementum nisi. Feugiat nibh sed pulvinar proin. At auctor urna nunc id. Convallis a cras semper auctor neque vitae tempus quam pellentesque.</p> 
-							<button id="vise">U ponudi...</button>
+							
 						</div>
-						<div class ="col-md-4">
+						<div class ="col-lg-4 col-md-4 col-xs-12">
 							<h3>Za zimnicu</h3>
 							<img src="https://www.broskva.hr/wp-content/uploads/2018/07/IMG_20181015_141027-300x300.jpg ">
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque ornare aenean euismod elementum nisi. Feugiat nibh sed pulvinar proin. At auctor urna nunc id. Convallis a cras semper auctor neque vitae tempus quam pellentesque.</p>
-							<button id="vise">U ponudi..</button>
+							
 						</div>
 				
 			</div>
