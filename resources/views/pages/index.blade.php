5178af7d90390a1c1f34a8572a0599ea5294dae8
AlmedinaBrasnjic <almedinabrasnjic@gmail.com>
2020-11-28 21:47:46 +0100

    ubacila sam svoj prvi test dio zadatka

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -48,6 +48,55 @@
             </div>
         </section>
 </div>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	
+		<div class="container">
+			<div class="row">
+			<div id="jedan" class="col-lg-6">
+			<div class="header">
+			<h1>Omiljeni recepti</h1>
+			</div>
+			
+			<div class="row">
+			<div id="korner" class="col-md-6">
+			<a href="humus" id="recept1" target="_blank"><h4>HUMUS</h4></a>
+			Humus je dokaz da zdrava hrana može biti jednostavna za pripremu. 
+			</div>
+			<div id="korner" class="col-md-6">
+			<h5>Krem juha od tikve</h5>
+			Boja bundeve ukazuje da je ova namirnica bogat izvor beta karotena.
+			
+			</div>
+			<div id="korner" class="col-md-6">
+			<h5>Mediteranski Obrok</h5>
+			Užitak u spremanju brzog i jednostavnog obroka će vam olakšati dan.
+			</div>
+			<div id="korner" class="col-md-6">
+			<h5>Ukusan citrus dressing za salatu</h5>
+			Mladi luk dodaje okus i boju ovom dresingu, ali može dati i malo poleta vašem napitku.
+			</div>
+			</div>
+			<div class="row">
+			<div id="korner" class="col-md-6">
+			<h5>Osvježavajuća i kremasta supa od graška</h5>
+			Grašak sadrži dvostruko više proteina u odnosu na ostalo povrće.
+			</div>
+			<div id="korner" class="col-md-6">
+			<h5>Krem supa od brokule</h5>
+			 Ova ukusna, bogata supa pružit će vam zdravlje i sreću - bez dodatnih mliječnih i zasićenih masti
+			- i  nećete moći vjerovati koliko je divna i kremasta.
+			</div>
+			
+			</div>
+		
+			</div>
+
+			<div id="jedan" class="col-lg-6">
+			<img src="https://images.unsplash.com/photo-1506458961255-571f40df5aad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80" height="530" width="600">
+			</div>
+			</div>
+			<button id="dugmic">SVI RECEPTI</button>
+		</div>
 
     <div class="footer-full">
         @include('partials.footer')
@@ -141,6 +190,44 @@
             background-position: calc(100% - 50px) center;
 
             }
+            body{
+		background-color:#f5edf1;
+	}
+	.header{
+		font-style:bold;
+		color:#635e52;
+	}
+	#korner{
+		margin-top:7%;
+		background-size:5%;
+		font-size:15px;
+		
+		
+	}
+	#recepti div.recepti{
+		display: block;
+		float: right;
+		width: 475px;
+		margin: 5;
+		padding: 3;
+	}
+	#jedan{
+		margin-top:3%;
+	}
+	#dugmic{
+		
+		color: violet;
+		padding: 15px 32px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
+		margin: 4px 2px;
+		cursor: pointer;
+	}
+	#recepti1 a:hover {
+		background-color:red;
+	}
     </style>
 
 @endsection
