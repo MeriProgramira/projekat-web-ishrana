7775338fb4c6c69d0817fdc42bccf3ec3a9f0449
Marushy <mersiha.programira@gmail.com>
2020-12-03 14:23:17 +0100

    changed footer_links

---
+++ resources/views/partials/footer_links.blade.php
@@ -0,0 +1,35 @@
+<div class="container-fluid footer" id="kontakt">
+
+
+    <div class="row  mx-3 contact align-content-center">
+        <div class="col-md-4">
+            <h2>ostanimo u kontaktu</h2>
+        </div>
+        <div class="col-md-4">
+            <h5 class="text-uppercase">telefon</h5>
+            <p>123 456-789</p>
+        </div>
+        <div class="col-md-4">
+            <h5 class="text-uppercase">email</h5>
+            <p>info@zdravaishrana.com</p>
+        </div>
+
+    </div>
+</div>
+
+
+<style>
+
+
+    .footer {
+        height: 100vh;
+    }
+
+    .contact {
+        height: 25vh;
+    }
+
+    h2, h5 {
+        font-weight: 600;
+    }
+</style>
